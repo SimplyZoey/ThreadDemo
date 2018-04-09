beaa570acdc262e282b87ab829c05419faea59f3
shitian <shitian_mail@163.com>
2018-04-09 22:48:26 +0800

    死锁

--- src/main/java/com/rocky/thread/CacheManager.java
+++ src/main/java/com/rocky/thread/CacheManager.java
@@ -1,9 +1,63 @@
 package com.rocky.thread;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 /**
  * @Author: shtian
  * @Description:
  * @Date: Create in 2018/4/9 19:55
  */
 public class CacheManager {
+    private Map<String, String> cache = new HashMap<>();
+    private ReadWriteLock lock = new ReentrantReadWriteLock();
+
+    public static int THREAD_COUNT = 2;
+
+    public void fresh() {
+        synchronized (cache) {
+            cache.clear();
+        }
+    }
+
+    public void put(String key, String value) {
+//        synchronized (cache) {
+        lock.writeLock().lock();
+        cache.put(key, value);
+        System.out.println(Thread.currentThread().getName() + " cache.put----" + value);
+        lock.writeLock().unlock();
+//        }
+    }
+
+    public String get(String key) {
+        String result = null;
+//        synchronized (cache) {
+        lock.readLock().lock();
+        result = cache.get(key);
+        System.out.println(Thread.currentThread().getName() + " cache.get-----" + result);
+        lock.readLock().unlock();
+//        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        final CacheManager manager = new CacheManager();
+        Thread[] threads = new Thread[THREAD_COUNT];
+        for (int i = 0; i < THREAD_COUNT; i++) {
+            threads[i] = new Thread(() -> {
+                for (int j = 0; j < 10; j++) {
+                    manager.put("test_key", "value_" + j);
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    manager.get("test_key");
+                }
+            });
+            threads[i].start();
+        }
+    }
 }
---
+++ src/main/java/com/rocky/thread/lock/DeadLock.java
@@ -0,0 +1,45 @@
+package com.rocky.thread.lock;
+
+/**
+ * @Author: shtian
+ * @Description:
+ * @Date: Create in 2018/4/9 21:31
+ */
+public class DeadLock {
+
+    public static void main(String[] args) {
+        Object a = new Object();
+        Object b = new Object();
+        System.out.println("lock a is : " + a.toString());
+        System.out.println("lock b is : " + b.toString());
+        Thread thread1 = new Thread(new Task(a, b));
+        Thread thread2 = new Thread(new Task(b, a));
+        thread1.start();
+        thread2.start();
+    }
+
+}
+
+class Task implements Runnable {
+    private Object lock_a;
+    private Object lock_b;
+
+    public Task(Object a, Object b) {
+        this.lock_a = a;
+        this.lock_b = b;
+    }
+
+    public void doSomething() {
+        synchronized (lock_a) {
+            System.out.println(Thread.currentThread().getName() + " get lock :" + lock_a.toString() + " want get lock :" + lock_b.toString());
+            synchronized (lock_b) {
+                System.out.println(Thread.currentThread().getName() + "dosomething.......");
+            }
+        }
+    }
+
+    @Override
+    public void run() {
+        doSomething();
+    }
+}
