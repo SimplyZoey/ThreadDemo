e2c5776bb93634175432f2dacf729b0563583f97
shitian <shitian_mail@163.com>
2018-04-26 11:14:44 +0800

    文档

--- src/main/java/com/rocky/thread/threadlocal/CountTest.java
+++ src/main/java/com/rocky/thread/threadlocal/CountTest.java
@@ -7,9 +7,9 @@ public class CountTest {
     public static void main(String[] args) throws Exception {
         Thread thread;
         //由于线程内部变量共享，导致counter计数非安全
-//		CountThread counter = new CountThread();
+		CountThread counter = new CountThread();
         //线程安全，变量为每个线程私有
-        SafeCountThread counter = new SafeCountThread();
+//        SafeCountThread counter = new SafeCountThread();
         for (int i = 0; i < 2; i++) {
             thread = new Thread(counter);
             thread.start();
--- src/main/java/com/rocky/thread/threadlocal/CountThread.java
+++ src/main/java/com/rocky/thread/threadlocal/CountThread.java
@@ -1,14 +1,18 @@
 package com.rocky.thread.threadlocal;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class CountThread implements Runnable {
 
-    private int counter = 0;
+//    private volatile int counter = 0;
+    private volatile AtomicInteger counter = new AtomicInteger();
 
     @Override
     public void run() {
         for (int i = 0; i < 10; i++) {
-            counter++;
-            System.out.println(Thread.currentThread().getName() + ":" + counter);
+//            counter++;
+            int result = counter.incrementAndGet();
+            System.out.println(Thread.currentThread().getName() + ":" + result);
         }
 
     }
